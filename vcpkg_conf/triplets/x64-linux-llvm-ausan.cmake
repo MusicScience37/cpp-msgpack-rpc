7291a84c3838cc9541404c8239a24ec91df6ae54
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-11-01 15:40:03 +0000

    :wrench: Update environment.

---
+++ vcpkg_conf/triplets/x64-linux-llvm-ausan.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CXX_FLAGS
+    "${VCPKG_CXX_FLAGS} -stdlib=libc++ -fsanitize=address,undefined")
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -fsanitize=address,undefined")
+set(VCPKG_LINKER_FLAGS "-fsanitize=address,undefined")
