493cbe96d14ce25ed238fc155f58579a08da80ce
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-06-23 04:11:43 +0000

    :white_check_mark: Test to use multiple clients for a server.

---
+++ tests/durability/test_multiple_clients.py
@@ -0,0 +1,92 @@
+"""Test to request from multiple clients."""
+
+import datetime
+import pathlib
+import shutil
+
+from .process_executor import ProcessExecutor
+
+
+def test_multiple_clients_tcp_v4(
+    bin_dir_path: pathlib.Path,
+    log_parent_dir_path: pathlib.Path,
+    test_duration: datetime.timedelta,
+) -> None:
+    """Test to request from multiple clients (TCPv4)."""
+    current_log_dir_path = log_parent_dir_path / "test_multiple_clients_tcp_v4"
+    if current_log_dir_path.exists():
+        shutil.rmtree(str(current_log_dir_path))
+    current_log_dir_path.mkdir(exist_ok=True, parents=True)
+
+    server_uri = "tcp://127.0.0.1:12345"
+
+    with ProcessExecutor(
+        [
+            str(bin_dir_path / "durability_serve_methods"),
+            "--uri",
+            server_uri,
+            "--log",
+            "server.log",
+        ],
+        cwd=str(current_log_dir_path),
+        log_prefix="server",
+    ) as server_process:
+        with ProcessExecutor(
+            [
+                str(bin_dir_path / "durability_request_continuously"),
+                "--uri",
+                server_uri,
+                "--log",
+                "client1.log",
+                "--seconds",
+                str(int(test_duration.total_seconds())),
+            ],
+            cwd=str(current_log_dir_path),
+            log_prefix="client1",
+        ) as client1_process:
+            with ProcessExecutor(
+                [
+                    str(bin_dir_path / "durability_request_continuously"),
+                    "--uri",
+                    server_uri,
+                    "--log",
+                    "client2.log",
+                    "--seconds",
+                    str(int(test_duration.total_seconds())),
+                ],
+                cwd=str(current_log_dir_path),
+                log_prefix="client2",
+            ) as client2_process:
+                with ProcessExecutor(
+                    [
+                        str(bin_dir_path / "durability_request_continuously"),
+                        "--uri",
+                        server_uri,
+                        "--log",
+                        "client3.log",
+                        "--seconds",
+                        str(int(test_duration.total_seconds())),
+                    ],
+                    cwd=str(current_log_dir_path),
+                    log_prefix="client3",
+                ) as client3_process:
+                    with ProcessExecutor(
+                        [
+                            str(bin_dir_path / "durability_request_continuously"),
+                            "--uri",
+                            server_uri,
+                            "--log",
+                            "client4.log",
+                            "--seconds",
+                            str(int(test_duration.total_seconds())),
+                        ],
+                        cwd=str(current_log_dir_path),
+                        log_prefix="client4",
+                    ) as client4_process:
+                        client4_process.wait(test_duration.total_seconds() * 2.0)
+                    client3_process.wait(1.0)
+                client2_process.wait(1.0)
+            client1_process.wait(1.0)
+
+    assert client1_process.returncode == 0
+    assert server_process.returncode == 0
