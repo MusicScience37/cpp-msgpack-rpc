76ee57bc2ef9eaf31d39c7a33d069399462c2ba3
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-08-04 13:09:07 +0000

    :bug: Fix build with the latest version of fmt library.

--- tests/integ/client/many_calls_test.cpp
+++ tests/integ/client/many_calls_test.cpp
@@ -26,6 +26,7 @@
 #include <catch2/catch_message.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 
 #include "create_test_logger.h"
 #include "msgpack_rpc/addresses/uri.h"
--- tests/integ/server/use_server_test.cpp
+++ tests/integ/server/use_server_test.cpp
@@ -31,6 +31,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_tostring.hpp>
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 
 #include "../transport/transport_helper.h"
 #include "create_test_logger.h"
