6f6db7651713e49819777800dbbda60912aeb4b1
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-08-13 02:43:32 +0000

    :white_check_mark: Durability tests of Unix sockets.

--- tests/durability/test_request_continuously.py
+++ tests/durability/test_request_continuously.py
@@ -9,19 +9,14 @@ from .process_executor import DEFAULT_PROCESS_WAIT_TIME, ProcessExecutor
 THIS_DIR = pathlib.Path(__file__).absolute().parent
 
 
-def test_request_continuously_tcp_v4(
+def _check_request_continuously(
+    *,
     bin_dir_path: pathlib.Path,
-    log_parent_dir_path: pathlib.Path,
+    current_log_dir_path: pathlib.Path,
     test_duration: datetime.timedelta,
+    server_uri: str,
 ) -> None:
-    """Test to request continuously (TCPv4)."""
-    current_log_dir_path = log_parent_dir_path / "test_request_continuously_tcp_v4"
-    if current_log_dir_path.exists():
-        shutil.rmtree(str(current_log_dir_path))
-    current_log_dir_path.mkdir(exist_ok=True, parents=True)
-
     config_file_path = THIS_DIR / "minimal_config.toml"
-    server_uri = "tcp://127.0.0.1:12345"
 
     with ProcessExecutor(
         [
@@ -57,3 +52,45 @@ def test_request_continuously_tcp_v4(
 
     assert client_process.returncode == 0
     assert server_process.returncode == 0
+
+
+def test_request_continuously_tcp_v4(
+    bin_dir_path: pathlib.Path,
+    log_parent_dir_path: pathlib.Path,
+    test_duration: datetime.timedelta,
+) -> None:
+    """Test to request continuously (TCPv4)."""
+    current_log_dir_path = log_parent_dir_path / "test_request_continuously_tcp_v4"
+    if current_log_dir_path.exists():
+        shutil.rmtree(str(current_log_dir_path))
+    current_log_dir_path.mkdir(exist_ok=True, parents=True)
+
+    server_uri = "tcp://127.0.0.1:12345"
+
+    _check_request_continuously(
+        bin_dir_path=bin_dir_path,
+        current_log_dir_path=current_log_dir_path,
+        test_duration=test_duration,
+        server_uri=server_uri,
+    )
+
+
+def test_request_continuously_unix(
+    bin_dir_path: pathlib.Path,
+    log_parent_dir_path: pathlib.Path,
+    test_duration: datetime.timedelta,
+) -> None:
+    """Test to request continuously (Unix socket)."""
+    current_log_dir_path = log_parent_dir_path / "test_request_continuously_unix"
+    if current_log_dir_path.exists():
+        shutil.rmtree(str(current_log_dir_path))
+    current_log_dir_path.mkdir(exist_ok=True, parents=True)
+
+    server_uri = "unix://durability_request_continuous.sock"
+
+    _check_request_continuously(
+        bin_dir_path=bin_dir_path,
+        current_log_dir_path=current_log_dir_path,
+        test_duration=test_duration,
+        server_uri=server_uri,
+    )
