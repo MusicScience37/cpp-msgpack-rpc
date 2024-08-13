c1ec54b57fbacaf8e76039b9ad71416e6602ee4f
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-08-13 05:38:11 +0000

    :alembic: Measure time of make_shared function.

---
+++ tests/bench/memory/shared_objects.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Benchmark of memory management of shared objects.
+ */
+#include <memory>
+
+#include <stat_bench/benchmark_macros.h>
+#include <stat_bench/do_not_optimize.h>
+
+struct Object {
+    std::size_t num{0};
+};
+
+STAT_BENCH_CASE("shared_objects", "make_shared") {
+    STAT_BENCH_MEASURE() {
+        stat_bench::do_not_optimize(std::make_shared<Object>());
+    };
+}
+
+STAT_BENCH_MAIN
